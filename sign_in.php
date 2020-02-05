a9400b13894a00b9e791f587828d65476dc0ef42
Luka Mugosa <lukamugosa1945@gmail.com>
2020-02-05 19:57:00 +0100

    Adds beginig of my hobby project

---
+++ sign_in.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Courgette|Pacifico:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/signin.css">
+    <title>Sign Up!</title>
+  </head>
+  <body>
+
+    <div class="singin-form">
+        <form action="" method="post">
+            <div class="form-header">
+                <h2>Sign In</h2>
+                <p>Login to MyChat</p>
+            </div>
+            <div class="form-group">
+                <label >Email:</label>
+                <input class="form-control" type="email" name="email" placeholder="someone@site.com" autocomplete="off" required>
+
+            </div>
+            <div class="form-group">
+                <label >Password:</label>
+                <input class="form-control" type="password" name="password" placeholder="Password" autocomplete="off" required>
+            </div>
+            <div class="small">Forgot password?<a href="forgot_pass.php">Click Here</a></div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block btn-lg" name="sign_in">Sign In</button>
+            </div>
+            <!-- <?php include("signin_user.php");?> -->
+        </form>
+        <div class="text-center small" style="color:#674288;">Don't have an account? <a href="sign_up.php">Create one!</a></div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
